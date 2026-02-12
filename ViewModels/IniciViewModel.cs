548ed693ba1145fd055da932a3ee4799edd553f6
deivid_p <deividmichael.ruiz@uvic.cat>
2026-02-12 17:54:52 +0100

    IniciViewModel Finished

---
+++ ViewModels/IniciViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WPF_MVVM_SPA_Template.Models;
+
+namespace WPF_MVVM_SPA_Template.ViewModels
+{
+    //Els ViewModels deriven de INotifyPropertyChanged per poder fer Binding de propietats
+    class IniciViewModel : INotifyPropertyChanged
+    {
+        // Referència al ViewModel principal
+        private readonly MainViewModel _mainViewModel;
+  
+        public IniciViewModel(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+
+
+        }
+
+
+        // Això és essencial per fer funcionar el Binding de propietats entre Vistes i ViewModels
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
