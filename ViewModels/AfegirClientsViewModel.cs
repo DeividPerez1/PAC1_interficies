b715513c09a99b448774c07acee2fb4e86fe3f50
deivid_p <deividmichael.ruiz@uvic.cat>
2026-02-12 20:29:21 +0100

    Logica para el formulario implementada

--- ViewModels/AfegirClientsViewModel.cs
+++ ViewModels/AfegirClientsViewModel.cs
@@ -15,24 +15,45 @@ namespace WPF_MVVM_SPA_Template.ViewModels
         // ObservableCollection és una llista que notifica els canvis a la vista
         public ObservableCollection<Client> Clients { get; set; } = new ObservableCollection<Client>();
 
-        // Propietat per controlar l'estudiant seleccionat a la vista
-        private Client _selectedClient;
-        public Client SelectedClient
+        // Propietat per controlar seleccionat a la vista
+        private Client _NewClient;
+        public Client NewClient
         {
-            get { return _selectedClient; }
-            set { _selectedClient = value; OnPropertyChanged(); }
+
+            get => _NewClient;
+            set { _NewClient = value; OnPropertyChanged(); }
         }
 
         // RelayCommands connectats via Binding als botons de la vista
-        public RelayCommand AddClientCommand { get; set; }
-        public RelayCommand DelClientCommand { get; set; }
-        public RelayCommand VeureGraficaCommand { get; set; }
+        public RelayCommand SaveCommand { get; set; }
+        public RelayCommand CancelCommand { get; set; }
+ 
         public AfegirClientsViewModel(MainViewModel mainViewModel)
         {
             _mainViewModel = mainViewModel;
 
+            _NewClient = new Client();
+            // Lógica del comando Guardar
+            SaveCommand = new RelayCommand(x => SaveClient());
 
- 
+            // Lógica del comando Cancelar (volver atrás)
+            CancelCommand = new RelayCommand(x => _mainViewModel.SelectedView = "Option1");
+
+        }
+
+        private void SaveClient()
+        {
+
+            if (NewClient == null) return;
+            //  Generar un ID automático basado en el último de la lista
+            int nextId = _mainViewModel.Option1VM.Clients.Count + 1;
+            NewClient.Id = nextId;
+
+            //  Añadir el cliente a la ObservableCollection de Option1ViewModel
+            _mainViewModel.Option1VM.Clients.Add(NewClient);
+
+            //  Navegar de vuelta a la lista de clientes
+            _mainViewModel.SelectedView = "Option1";
         }
 
         // Això és essencial per fer funcionar el Binding de propietats entre Vistes i ViewModels
