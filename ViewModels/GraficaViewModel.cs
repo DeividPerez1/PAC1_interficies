8aa0fccafb867cf225052a7b1bb1f40401b81bb9
deivid_p <deividmichael.ruiz@uvic.cat>
2026-02-20 18:58:46 +0100

    AÑADICION DE LAS PARTES DE LA GRAFICA

---
+++ ViewModels/GraficaViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq; // <--- IMPORTANT: Necessari per fer cerques (FirstOrDefault)
+using WPF_MVVM_SPA_Template.Models;
+
+namespace WPF_MVVM_SPA_Template.ViewModels
+{
+    class GraficaViewModel : INotifyPropertyChanged
+    {
+        private readonly MainViewModel _mainViewModel;
+
+
+        public GraficaViewModel(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+
+
+        }
+
+
+
+        // Això és essencial per fer funcionar el Binding de propietats entre Vistes i ViewModels
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Views/GraficaView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+namespace WPF_MVVM_SPA_Template.Views
+{
+    // Vista UserControl enlloc de Window ja que no necessitem una finestra (SPA)
+    public partial class GraficaView : UserControl
+    {
+        public GraficaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
