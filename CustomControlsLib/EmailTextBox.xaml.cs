44ea6c5f25e50baaa7cbbe8d0271856075acebad
deivid_p <deividmichael.ruiz@uvic.cat>
2026-03-13 20:46:08 +0100

    Validació de Email
    Esta sin acabar

--- CustomControlsLib/EmailTextBox.xaml.cs
+++ CustomControlsLib/EmailTextBox.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,44 @@ namespace CustomControlsLib
     /// </summary>
     public partial class EmailTextBox : UserControl
     {
+        public static readonly DependencyProperty EmailTextProperty =
+            DependencyProperty.Register(
+                   "EmailText",
+                   typeof(string),
+                   typeof(EmailTextBox),
+                   new FrameworkPropertyMetadata(
+                       string.Empty,
+                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                       OnEmailtextChanged));
+        public string EmailText
+        {
+            get => (string)GetValue(EmailTextProperty);
+            set => SetValue(EmailTextProperty, value);
+        }
+
         public EmailTextBox()
         {
             InitializeComponent();
+            InnerTextBox.TextChanged += (s, e) => EmailText = InnerTextBox.Text;
+        }
+        private static void OnEmailtextChanged(DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+        {
+            var control = (EmailTextBox)d;
+            string newEmail = (string)e.NewValue;
+            bool isValid = IsValid(newEmail);
+        }
+
+        public static bool IsValid(string email)
+        {
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
         }
     }
 }
