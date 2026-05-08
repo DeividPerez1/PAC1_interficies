2aedaca584fc0caf8130b95a1842759926b195a6
deivid_p <deividmichael.ruiz@uvic.cat>
2026-05-08 12:05:26 +0200

    Canvis en el visual de l'informe

--- CustomControlsLib/obj/Debug/net8.0-windows/CustomControlsLib.AssemblyInfo.cs
+++ CustomControlsLib/obj/Debug/net8.0-windows/CustomControlsLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomControlsLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d38dfedda221926cb51d7362bd5e82e566817c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ec5ca3c2acc70058462baf60eedad6219c6f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomControlsLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomControlsLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
